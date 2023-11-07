530c52c4cf55d7dfe10e735e40305efab34cc716
Midgard <malmeidamdam1@gmail.com>
2023-11-07 14:10:30 +0100

    COMMIT

--- Formulario WPF Midgard Almeida/Formulario WPF Midgard Almeida/MainWindow.xaml.cs
+++ Formulario WPF Midgard Almeida/Formulario WPF Midgard Almeida/MainWindow.xaml.cs
@@ -32,22 +32,60 @@ namespace Formulario_WPF_Midgard_Almeida
             openFileDialog1.Filter = "Archivos de imagen|*.jpg;*.jpeg;*.png;*.bmp|Todos los archivos|*.*";
             if (openFileDialog1.ShowDialog() == true)
             {
-                // Obtén la ruta del archivo seleccionado
+                // Obtener la ruta de la imagen elegida por usuario 
                 string rutaImagen = openFileDialog1.FileName;
 
-                // Actualiza la imagen en el control Image
+                // Actualizar la imagen en 
                 BitmapImage bitmapImage = new BitmapImage(new Uri(rutaImagen));
                 imgFoto.Source = bitmapImage;
+
+                
             }
         }
-        private void cambiarTexto() {
-
-           // IsNullOrWhiteSpace(txtDireccion);
 
 
+        private void Txt_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (sender is TextBox textBox) {
+                if (textBox.Text == "Direccion" || textBox.Text == "Ciudad" || textBox.Text == "Provincia" || textBox.Text == "Codio Postal" || textBox.Text == "Pais")
+                {
+                    textBox.Text = "";
+                }
+            }
+        }
+
+
+        private void Txt_lostFocus(object sender, RoutedEventArgs e)
+        {
+            if (sender is TextBox textBox)
+            {
+                if (string.IsNullOrWhiteSpace(textBox.Text))
+                {
+                    //if (textBox.Name == "txtDireccion")
+
+
+                }
+
+            }
+        }
+
+
+            private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Cogemos el texto de los textBox, los metemos en variables
+            //Las variables son añadidas al dataGridColumn donde su Binding="{Binding Apellidos}"
+            //Lo que junta es el valor de binding en el dataGrid 
+            //Para recordar esto dejo al dataGrid de Email con nombre X
+
+            string nombre = txtNombre.Text;
+            string apellidos = txtApellidos.Text;
+            string email = txtEmail.Text;
+            string telefono = txtTelefono.Text;
+
+            dataGrid.Items.Add(new { Nombre = nombre, Apellidos = apellidos, X = email, Telefono = telefono });
 
         }
-       
-    }
 
+
+    }
 }
